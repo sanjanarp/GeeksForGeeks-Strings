d53c7c64159b6fcb1facd6121b2c7879363fc563
Sanjana RP <sanjurps100@gmail.com>
2021-09-25 14:19:49 +0530

    Update Print Anagrams Together.java

--- Print Anagrams Together.java
+++ Print Anagrams Together.java
@@ -1,12 +1,12 @@
 class Solution {
     public List<List<String>> Anagrams(String[] s) {
-        // Code here
         List<List<String>> res = new ArrayList<>(); 
         HashMap<String, ArrayList<String>> map = new HashMap<>();
         for(int i=0;i<s.length;i++){
             char[] temp = s[i].toCharArray();
             Arrays.sort(temp);
               if(!map.containsKey(String.valueOf(temp))){
+                  //if map contains the required string, then keep adding in all the variations of that string available in the given list as values in the map
                  ArrayList<String> al = new ArrayList<>();
                  al.add(s[i]);
                 map.put(String.valueOf(temp),al);
